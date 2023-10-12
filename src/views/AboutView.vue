e33d615a8e5573e45db9bb17b76abc78980764a7
mjlaudencia <laudenciamj3@gmail.com>
2023-10-13 00:03:46 +0800

    rawr

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,10 +6,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/about">Manage</a></li>
                         <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
+        <h1>This is manage</h1>
 </template>
