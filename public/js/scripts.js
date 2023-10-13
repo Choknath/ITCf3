a0df95358b853778787e46c76b7f14eb7423dbb9
Choknath <130685008+Choknath@users.noreply.github.com>
2023-10-12 23:06:33 -0700

    change

---
+++ public/js/scripts.js
@@ -0,0 +1,12 @@
+/*!
+* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+<template>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/scripts.js"></script>
+</template>
